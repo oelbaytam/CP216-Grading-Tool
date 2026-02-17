678a02b1039fa3ac128f34479810d748184dadce
oelbaytam <oelbaytam@gmail.com>
2026-02-16 22:34:41 -0500

    Updates

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface StudentSubmission {
-  studentId: string;
-  studentName: string;
-  studentCode: string;
-  zipFilename: string;
-  rawZipBlob: Blob;
-  files: Record<string, string>;
-}
